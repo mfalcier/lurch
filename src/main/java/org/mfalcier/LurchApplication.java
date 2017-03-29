02d89053d95cb5da038728b629a3070510a83ad3
mfalcier <marcofalcier@gmail.com>
2017-03-29 09:25:32 +0200

    :tada: Initial Commit

---
+++ src/main/java/org/mfalcier/LurchApplication.java
@@ -0,0 +1,12 @@
+package org.mfalcier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LurchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LurchApplication.class, args);
+	}
+}
